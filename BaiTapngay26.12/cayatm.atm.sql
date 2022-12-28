4c7d642be94f875af3d33c3645592a316c3983bc
phuonggtp2409 <121173523+phuonggtp2409@users.noreply.github.com>
2022-12-29 00:57:51 +0700

    update 2

---
+++ BaiTapngay26.12/cayatm.atm.sql
@@ -0,0 +1,10 @@
+SELECT * FROM atm.cayatm;
+INSERT INTO cayatm (maCayATM, maNganHang, diaChi)
+VALUES ('VIBCG1', 'VIB', '139 Cau Giay, Ha Noi');
+SELECT * FROM atm.cayatm;
+INSERT INTO cayatm (maCayATM, maNganHang, diaChi)
+VALUES ('VIBDD1', 'VIB', '40 Ton That Tung, Dong Da, Ha Noi');
+SELECT * FROM atm.cayatm;
+INSERT INTO cayatm (maCayATM, maNganHang, diaChi)
+VALUES ('BIDVCG1', 'BIDV', '139 Cau Giay, Ha Noi');
+SELECT * FROM atm.cayatm;
---
+++ BaiTapngay26.12/nganhang.atm.sql
@@ -0,0 +1,10 @@
+SELECT * FROM atm.nganhang;
+INSERT INTO nganhang (maNganHang, tenNganHang, chiNhanh)
+VALUES ('BIDV', 'Ngan hang thuong mai co phan dau tu va phat trien Viet Nam', 'Dong Da');
+SELECT * FROM atm.nganhang;
+INSERT INTO nganhang (maNganHang, tenNganHang, chiNhanh)
+VALUES ('VIB', 'Ngan hang quoc te', 'Dong Da');
+SELECT * FROM atm.nganhang;
+INSERT INTO nganhang (maNganHang, tenNganHang, chiNhanh)
+VALUES ('VIB', 'Ngan hang quoc te', 'Cau Giay');
+SELECT * FROM atm.nganhang;
