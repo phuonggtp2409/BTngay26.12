f451e46a9929290ee8af469849355fa63469a3c3
phuonggtp2409 <121173523+phuonggtp2409@users.noreply.github.com>
2022-12-29 00:21:51 +0700

    ATM-BT26.12

---
+++ BaiTapngay26.12/chuthe.atm.sql
@@ -0,0 +1,10 @@
+-- SELECT * FROM atm.chutheatm;
+-- INSERT INTO chutheatm (maChuThe, hoTen, ngayThangNamSinh, CCCD, SDT, diaChi)
+-- VALUES ('1', 'Phuong A', '1999-08-15', '122281083', '0966063780', 'BG');
+-- SELECT * FROM atm.chutheatm;
+-- INSERT INTO chutheatm (maChuThe, hoTen, ngayThangNamSinh, CCCD, SDT, diaChi)
+-- VALUES ('2', 'Phuong B', '1998-08-15', '122281084', '0966063781', 'BG');
+-- SELECT * FROM atm.chutheatm;
+-- INSERT INTO chutheatm (maChuThe, hoTen, ngayThangNamSinh, CCCD, SDT, diaChi)
+-- VALUES ('3', 'Phuong C', '1997-08-15', '122281085', '0966063782', 'BG');
+SELECT * FROM atm.chutheatm;
---
+++ BaiTapngay26.12/theatm.atm.sql
@@ -0,0 +1,10 @@
+-- SELECT * FROM atm.theatm;
+-- INSERT INTO theatm (maThe, maChuThe, soThe, ngayPhatHanh, nganHang)
+-- VALUES ('T1', '1', '12345', '2022-10-10', 'BIDV');
+-- SELECT * FROM atm.theatm;
+-- INSERT INTO theatm (maThe, maChuThe, soThe, ngayPhatHanh, nganHang)
+-- VALUES ('T2', '1', '12346', '2022-11-10', 'VIB');
+-- SELECT * FROM atm.theatm;
+-- INSERT INTO theatm (maThe, maChuThe, soThe, ngayPhatHanh, nganHang)
+-- VALUES ('T3', '2', '12347', '2022-12-10', 'VIB');
+SELECT * FROM atm.theatm;
