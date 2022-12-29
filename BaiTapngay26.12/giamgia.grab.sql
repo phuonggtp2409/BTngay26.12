c439775a982c5a6ea2d8e7d6e84682b1107e97b6
phuonggtp2409 <121173523+phuonggtp2409@users.noreply.github.com>
2022-12-30 00:06:03 +0700

    update 30.12

---
+++ BaiTapngay26.12/giamgia.grab.sql
@@ -0,0 +1,10 @@
+--  CREATE TABLE grab.giamgia (
+--  maGiamGia varchar(225),
+--  ndGiamGia varchar(225),
+--  soTienGiam varchar(225))
+-- SELECT * FROM grab.giamgia;
+-- INSERT INTO giamgia (maGiamGia, ndGiamGia, soTienGiam)
+-- VALUES ('MGG10','giam gia 10.000VND cho chuyen di dau tien','10.000VND'),
+-- ('MGG15','giam gia 15.000VND cho chuyen di thu hai','15.000VND'),
+-- ('MGG20','giam gia 20.000VND khi thanh toan chuyen khoan','20.000VND')
+SELECT * FROM grab.giamgia;
